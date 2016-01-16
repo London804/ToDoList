3fd89ad21bb88c42006d4ab3fbe939b4a823e7f7
Alex Chaparro <alex@kargo.com>
2016-01-16 17:46:19 -0500

    sass is clean and controllers are set up

--- dist/all.js
+++ dist/all.js
@@ -23,9 +23,15 @@ myApp.config(function($routeProvider){
 myApp.controller('mainController', ['$scope', function($scope) {
 //the logic for main.html will go here
 	$scope.name = 'Main';
+	
 }]);
 
 myApp.controller('secondController', ['$scope', function($scope) {
-//for second.html here
-	$scope.name = 'Second';
+//for d3.html here
+	$scope.name = 'd3';
+}]);
+
+myApp.controller('thirdController', ['$scope', function($scope) {
+//for neat.html here
+	$scope.name = 'neat';
 }]);
