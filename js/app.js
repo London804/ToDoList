7ef366dca7d85e98aae24aa2cbcc5aef3208209d
Alex Chaparro <alex@kargo.com>
2016-01-24 19:31:49 -0500

    clean up and basic functionality working

--- js/app.js
+++ js/app.js
@@ -5,27 +5,43 @@ myApp.config(function($routeProvider){
 	$routeProvider
 
 	.when('/', {
-		templateUrl: 'pages/main.html',
+		templateUrl: 'views/main.html',
 		controller: 'mainController'
 	})
 
 	.when('/d3', {
-		templateUrl: 'pages/d3.html',
+		templateUrl: 'views/d3.html',
 		controller: 'secondController'
 	})
 
 	.when('/neat', {
-		templateUrl: 'pages/neat.html',
+		templateUrl: 'views/neat.html',
 		controller: 'thirdController',
 	});
 });
 
 myApp.controller('mainController', ['$scope', function($scope) {
 //the logic for main.html will go here
-	$scope.name = 'Main';
+	$scope.todos = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+	$scope.name = 'main';
+
+	$scope.addTodo = function () {
+	  $scope.todos.push($scope.todo);
+	  $scope.todo = '';
+	};
+
+	$scope.removeTodo = function (index) {
+	  $scope.todos.splice(index, 1);
+	};
+
 }]);
 
 myApp.controller('secondController', ['$scope', function($scope) {
-//for second.html here
-	$scope.name = 'Second';
+//for d3.html here
+	$scope.name = 'd3';
+}]);
+
+myApp.controller('thirdController', ['$scope', function($scope) {
+//for neat.html here
+	$scope.name = 'neat';
 }]);
